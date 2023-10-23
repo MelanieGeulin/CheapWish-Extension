7c349412b1fda0598dc521b2336b6452608ee534
gloomykuriozity <gloomykuriozity@gmail.com>
2023-10-23 16:06:13 +0200

    Downloaded URL to file locally!

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
-
-  
+function collectUrls() {
+    const urls = [];
+    const items = document.querySelectorAll('a[data-testid^="product-item-id-"]');
+    for (const item of items) {
+      const url = item.getAttribute("href");
+      urls.push(url);
+    }
+    
+    // Send the collected URLs to the background script
+    chrome.runtime.sendMessage({ action: "logUrls", urls });
+  }
--- js/background.js
+++ js/background.js
@@ -1,30 +1,36 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log("Message received:", message);
-  
-    if (message.action === "collectUrls") {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        console.log("Tabs found:", tabs);
-        const activeTab = tabs[0];
-        if (activeTab) {
-          console.log("Active tab:", activeTab);
-          chrome.scripting.executeScript({
-            target: { tabId: activeTab.id },
-            function: collectUrls,
-          });
-        }
-      });
-    }
-  });
-  
-  
-  function collectUrls() {
-    console.log("Function entered: Yes");
-    const urls = [];
-    const items = document.querySelectorAll('a[data-testid^="product-item-id-"]');
-    for (const item of items) {
-      const url = item.getAttribute("href");
-      urls.push(url);
-    }
-    console.log(urls);
+  if (message.action === "collectUrls") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const activeTab = tabs[0];
+      if (activeTab) {
+        chrome.scripting.executeScript({
+          target: { tabId: activeTab.id },
+          function: collectUrls,
+        });
+      }
+    });
   }
-  
+
+  // Handle the URLs and create a text file for download
+  if (message.action === "logUrls") {
+    const urls = message.urls;
+    const urlsText = urls.join('\n'); // Convert the URLs to a text string
+
+    // Use chrome.downloads to initiate the download
+    chrome.downloads.download({
+      url: "data:text/plain;base64," + btoa(urlsText), // Encode the text as base64
+      filename: "collected_urls.txt",
+      saveAs: true,
+    });
+  }
+});
+
+function collectUrls() {
+  const urls = [];
+  const items = document.querySelectorAll('a[data-testid^="product-item-id-"]');
+  for (const item of items) {
+    const url = item.getAttribute("href");
+    urls.push(url);
+  }
+  chrome.runtime.sendMessage({ action: "logUrls", urls });
+}
